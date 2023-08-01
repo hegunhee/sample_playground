e2cf61c9078bb69f6e2a1505e9c06249326d4cff
hegunhee <gunhee0072@gmail.com>
2023-08-01 16:32:39 +0900

    [library-setting] feat : Hilt Application 설정 및 Activity에 Entrypoint 설정

---
+++ app/src/main/java/com/hegunhee/sample_playground/app/PlayGroundApp.kt
@@ -0,0 +1,8 @@
+package com.hegunhee.sample_playground.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PlayGroundApp : Application() {
+}
--- app/src/main/java/com/hegunhee/sample_playground/feature/MainActivity.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.hegunhee.sample_playground
+package com.hegunhee.sample_playground.feature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hegunhee.sample_playground.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
