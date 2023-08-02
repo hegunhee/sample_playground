035d92e0e3203eb3d9b10bac68eded5498540dde
hegunhee <gunhee0072@gmail.com>
2023-08-02 18:07:11 +0900

    [main-fragment] feat : SecurityKeyPadFragment kotlin 파일 작성

---
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadFragment.kt
@@ -0,0 +1,26 @@
+package com.hegunhee.sample_playground.feature.secretkeypad
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hegunhee.sample_playground.R
+import com.hegunhee.sample_playground.databinding.FragmentSecurityKeypadBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SecurityKeypadFragment : Fragment() {
+
+    private lateinit var viewDataBinding : FragmentSecurityKeypadBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_security_keypad,container,false)
+        viewDataBinding = FragmentSecurityKeypadBinding.bind(root)
+        return root
+    }
+}
