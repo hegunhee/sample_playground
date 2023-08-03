bbd83f892085c8b53962de7a2920217443d2855c
hegunhee <gunhee0072@gmail.com>
2023-08-03 16:42:17 +0900

    [security-keypad] feat : securityKeypadFragment toolBar 설정 및 네비게이션 연동

--- app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadFragment.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.hegunhee.sample_playground.R
 import com.hegunhee.sample_playground.databinding.FragmentSecurityKeypadBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,5 +24,10 @@ class SecurityKeypadFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_security_keypad,container,false)
         viewDataBinding = FragmentSecurityKeypadBinding.bind(root)
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewDataBinding.toolbar.setupWithNavController(findNavController())
     }
 }
