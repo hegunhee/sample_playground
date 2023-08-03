074bf978245456506836f58bbec5507f34e71367
hegunhee <gunhee0072@gmail.com>
2023-08-03 15:45:05 +0900

    [main-fragment] feat : navItem을 가져오는 코드 작성 및 navItem 클릭 이벤트 메서드 구현

--- app/src/main/java/com/hegunhee/sample_playground/feature/main/MainViewModel.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/main/MainViewModel.kt
@@ -1,14 +1,30 @@
 package com.hegunhee.sample_playground.feature.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel(), MainNavItemActionHandler{
-    
+
+    private val _navItemList : MutableStateFlow<List<MainNavItem>> = MutableStateFlow(getNavActionList())
+    val navItemList : StateFlow<List<MainNavItem>>
+        get() = _navItemList
+
+    private val _navigateId : MutableSharedFlow<Int> = MutableSharedFlow()
+    val navigateActionId : SharedFlow<Int>
+        get() = _navigateId
+
     override fun onClickNavItem(actionId: Int) {
-        TODO("Not yet implemented")
+        viewModelScope.launch {
+            _navigateId.emit(actionId)
+        }
     }
 
 
