099b5ab759de2eb6a8d5cd7d01a566c02e79388e
hegunhee <gunhee0072@gmail.com>
2023-08-03 15:57:10 +0900

    [main-fragment] feat : mainFragment에서 데이터 옵저빙, RecyclerView 초기화 코드 작성

--- app/src/main/java/com/hegunhee/sample_playground/feature/main/MainFragment.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/main/MainFragment.kt
@@ -5,26 +5,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.hegunhee.sample_playground.R
 import com.hegunhee.sample_playground.databinding.FragmentMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
 
     private lateinit var viewDataBinding : FragmentMainBinding
-
+    private val viewModel : MainViewModel by viewModels()
+    private lateinit var navItemAdapter : NavItemAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_main,container,false)
-        viewDataBinding = FragmentMainBinding.bind(root)
-
+        navItemAdapter = NavItemAdapter(viewModel)
+        viewDataBinding = FragmentMainBinding.bind(root).apply {
+            lifecycleOwner = viewLifecycleOwner
+            recyclerView.adapter = navItemAdapter
+        }
         return root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeData()
+    }
+
+    private fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.navItemList.collect{ itemList ->
+                        navItemAdapter.submitList(itemList)
+                    }
+                }
+                launch {
+                    viewModel.navigateActionId.collect { actionId ->
+                        findNavController().navigate(actionId)
+                    }
+                }
+            }
+        }
     }
 }
