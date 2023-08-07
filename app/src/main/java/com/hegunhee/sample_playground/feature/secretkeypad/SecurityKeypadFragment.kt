52640e84617a181dedc145db8d51a5ca4113be3c
hegunhee <gunhee0072@gmail.com>
2023-08-07 14:25:38 +0900

    [security-keypad] feat : fragment에 viewModel 주입 및 데이터바인딩으로 뷰모델 주입

--- app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadFragment.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.hegunhee.sample_playground.R
@@ -15,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class SecurityKeypadFragment : Fragment() {
 
     private lateinit var viewDataBinding : FragmentSecurityKeypadBinding
+    private val viewModel : SecurityKeypadViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +24,9 @@ class SecurityKeypadFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_security_keypad,container,false)
-        viewDataBinding = FragmentSecurityKeypadBinding.bind(root)
+        viewDataBinding = FragmentSecurityKeypadBinding.bind(root).apply {
+            viewModel = this@SecurityKeypadFragment.viewModel
+        }
         return root
     }
 
