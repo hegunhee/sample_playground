1b1383ffc8dcb4866a8ea629b556633a9bfffd18
hegunhee <gunhee0072@gmail.com>
2023-08-07 14:44:38 +0900

    [security-keypad] feat : 인증번호 등록 버튼 클릭리스너 데이터바인딩 연동

--- app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadViewModel.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadViewModel.kt
@@ -1,10 +1,24 @@
 package com.hegunhee.sample_playground.feature.secretkeypad
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SecurityKeypadViewModel @Inject constructor() : ViewModel() {
+
+    private val _keypadNavigation : MutableSharedFlow<KeypadType> = MutableSharedFlow<KeypadType>()
+    val keypadNavigation : SharedFlow<KeypadType> = _keypadNavigation.asSharedFlow()
+
+    fun onPasswordRegisterButtonClick() {
+        viewModelScope.launch {
+            _keypadNavigation.emit(KeypadType.Register())
+        }
+    }
 
 }
