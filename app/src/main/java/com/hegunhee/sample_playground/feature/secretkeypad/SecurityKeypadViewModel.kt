1a44677338eccb1fb95d7bdbb27f45701ebe6c9a
hegunhee <gunhee0072@gmail.com>
2023-08-07 14:15:00 +0900

    [security-keypad] feat : SecurityKeypadViewModel 클래스 작성

---
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadViewModel.kt
@@ -0,0 +1,10 @@
+package com.hegunhee.sample_playground.feature.secretkeypad
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SecurityKeypadViewModel @Inject constructor() : ViewModel() {
+
+}
