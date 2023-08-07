54cda328de8bd050dddbdea79f8b7e95ff47b4c2
hegunhee <gunhee0072@gmail.com>
2023-08-07 14:25:05 +0900

    [security-keypad] feat : 인증번호 등록과 확인을 나누는 KeypadType 클래스 정의

---
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/KeypadType.kt
@@ -0,0 +1,8 @@
+package com.hegunhee.sample_playground.feature.secretkeypad
+
+sealed class KeypadType() {
+
+    data class Register(val name : String = "인증번호 등록") : KeypadType()
+
+    data class Check(val name : String = "인증번호 확인") : KeypadType()
+}
