59290ab2a90ea9447751728af926e762628d9528
hegunhee <gunhee0072@gmail.com>
2023-08-07 14:28:38 +0900

    [security-keypad] style : KeypadBottomSheet secretkeypad.dialog 패키지로이동

--- app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/dialog/KeypadBottomSheetDialogFragment.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/dialog/KeypadBottomSheetDialogFragment.kt
@@ -1,4 +1,4 @@
-package com.hegunhee.sample_playground.feature.secretkeypad
+package com.hegunhee.sample_playground.feature.secretkeypad.dialog
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,8 +7,9 @@ import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.hegunhee.sample_playground.R
 import com.hegunhee.sample_playground.databinding.DialogBottomsheetKeypadBinding
+import com.hegunhee.sample_playground.feature.secretkeypad.KeypadType
 
-class KeypadBottomSheetDialogFragment : BottomSheetDialogFragment(){
+class KeypadBottomSheetDialogFragment(val keypadType : KeypadType) : BottomSheetDialogFragment(){
 
     private lateinit var viewDataBinding : DialogBottomsheetKeypadBinding
     override fun onCreateView(
@@ -22,5 +23,14 @@ class KeypadBottomSheetDialogFragment : BottomSheetDialogFragment(){
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        const val TAG_REGISTER = "register"
+        const val TAG_CHECK = "check"
+
+        fun getRegisterTypeInstance() : KeypadBottomSheetDialogFragment {
+            return KeypadBottomSheetDialogFragment(KeypadType.Register())
+        }
     }
 }
