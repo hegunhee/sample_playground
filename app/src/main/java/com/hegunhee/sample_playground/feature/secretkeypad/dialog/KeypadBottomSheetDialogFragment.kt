d97434d2d738ca43fa4040633b2f58b741839869
hegunhee <gunhee0072@gmail.com>
2023-08-07 18:32:54 +0900

    [security-keypad] feat : keypadAdapter 연동

--- app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/dialog/KeypadBottomSheetDialogFragment.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/dialog/KeypadBottomSheetDialogFragment.kt
@@ -4,21 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.hegunhee.sample_playground.R
 import com.hegunhee.sample_playground.databinding.DialogBottomsheetKeypadBinding
+import com.hegunhee.sample_playground.feature.secretkeypad.KeypadAdapter
 import com.hegunhee.sample_playground.feature.secretkeypad.KeypadType
 
 class KeypadBottomSheetDialogFragment(val keypadType : KeypadType) : BottomSheetDialogFragment(){
 
     private lateinit var viewDataBinding : DialogBottomsheetKeypadBinding
+    private lateinit var keypadAdapter : KeypadAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        keypadAdapter = KeypadAdapter()
         val root = inflater.inflate(R.layout.dialog_bottomsheet_keypad,container,false)
-        viewDataBinding = DialogBottomsheetKeypadBinding.bind(root)
+        viewDataBinding = DialogBottomsheetKeypadBinding.bind(root).apply {
+            lifecycleOwner = viewLifecycleOwner
+            keypadRecyclerView.apply {
+                layoutManager = GridLayoutManager(requireContext(),3)
+                adapter = keypadAdapter
+            }
+        }
         return root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
