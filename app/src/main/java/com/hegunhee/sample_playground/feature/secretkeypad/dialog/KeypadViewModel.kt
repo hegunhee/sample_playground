27fb8b6845be654fc7f4352990904f61cb74d931
hegunhee <gunhee0072@gmail.com>
2023-08-08 17:20:27 +0900

    [security-keypad] feat : 키패드 actionHandler 구현 및 keypadList 생성 코드 작성

--- app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/dialog/KeypadViewModel.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/dialog/KeypadViewModel.kt
@@ -1,27 +1,66 @@
 package com.hegunhee.sample_playground.feature.secretkeypad.dialog
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class KeypadViewModel @Inject constructor() : ViewModel() {
+class KeypadViewModel @Inject constructor() : ViewModel(), KeypadActionHandler {
+
+    private val keypadDelIndex = 9
+    private val keypadDel = "del"
 
     private val _keypad : MutableStateFlow<List<String>> = MutableStateFlow(emptyList())
     val keypad : StateFlow<List<String>> = _keypad.asStateFlow()
 
-    private val delIndex = 9
+    private val _password : MutableStateFlow<String> = MutableStateFlow("")
+    val password : StateFlow<String> = _password.asStateFlow()
 
+    private val isPasswordTextSealed : MutableStateFlow<Boolean> = MutableStateFlow(false)
 
-    fun fetchKeypad() {
-        val keypadList = mutableListOf<String>().apply {
+    var passwordText : StateFlow<String> = MutableStateFlow("")
+
+    fun fetchData() {
+        fetchKeypad()
+        combineData()
+    }
+
+    private fun fetchKeypad() {
+        _keypad.value = getKeypad()
+    }
+
+    private fun getKeypad() : List<String> {
+        return mutableListOf<String>().apply {
             addAll((0..9).map { it.toString() })
             shuffle()
-            add(delIndex,"del")
+            add(keypadDelIndex,"del")
         }
-        _keypad.value = keypadList.toList()
+    }
+
+    private fun combineData() = viewModelScope.launch {
+        passwordText = password.combine(isPasswordTextSealed) { pw, sealed ->
+            return@combine if (sealed) {
+                pw.map { '*' }.toString()
+            } else {
+                pw
+            }
+        }.stateIn(viewModelScope)
+    }
+    override fun onClickKeypad(keypad: String) {
+        val currentPassword = password.value
+        if(keypad == keypadDel) {
+            if(currentPassword.isNotBlank()){
+                _password.value = currentPassword.substring(0,password.value.length-1)
+            }
+        }else{
+            _password.value = currentPassword + keypad
+        }
     }
 }
