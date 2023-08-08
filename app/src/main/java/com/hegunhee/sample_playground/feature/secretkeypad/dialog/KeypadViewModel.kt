885b1f132b1ad7c63773e6683c2dcde54c0139e4
hegunhee <gunhee0072@gmail.com>
2023-08-08 15:00:04 +0900

    [security-keypad] feat : viewModel에서 keypadList를 가져오고 상태홀더에 저장하는 코드 작성

--- app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/dialog/KeypadViewModel.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/dialog/KeypadViewModel.kt
@@ -1,4 +1,27 @@
 package com.hegunhee.sample_playground.feature.secretkeypad.dialog
 
-class KeypadViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class KeypadViewModel @Inject constructor() : ViewModel() {
+
+    private val _keypad : MutableStateFlow<List<String>> = MutableStateFlow(emptyList())
+    val keypad : StateFlow<List<String>> = _keypad.asStateFlow()
+
+    private val delIndex = 9
+
+
+    fun fetchKeypad() {
+        val keypadList = mutableListOf<String>().apply {
+            addAll((0..9).map { it.toString() })
+            shuffle()
+            add(delIndex,"del")
+        }
+        _keypad.value = keypadList.toList()
+    }
 }
