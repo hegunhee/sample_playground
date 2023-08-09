31f462fb84f4d3539253821322c1572618d5eb59
hegunhee <gunhee0072@gmail.com>
2023-08-09 18:09:08 +0900

    [security-keypad] feat : dialog로 비밀번호 받아오고 저장하는 코드 작성

--- app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadFragment.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -38,7 +39,16 @@ class SecurityKeypadFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewDataBinding.toolbar.setupWithNavController(findNavController())
+        setResultListener()
         observeData()
+    }
+
+    private fun setResultListener() {
+        setFragmentResultListener(KeypadBottomSheetDialogFragment.TAG_REGISTER) { key, bundle ->
+            bundle.getString(KeypadBottomSheetDialogFragment.KEY_PASSWORD)?.let {
+                viewModel.setPassword(it)
+            }
+        }
     }
 
     private fun observeData() {
