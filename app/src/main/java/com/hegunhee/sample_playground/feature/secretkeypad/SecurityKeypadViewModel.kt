5807b042fcc28e35c9972e1d4c80f9b7af588451
hegunhee <gunhee0072@gmail.com>
2023-08-09 18:09:08 +0900

    [security-keypad] feat : dialog로 비밀번호 받아오고 저장하는 코드 작성

--- app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadViewModel.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/SecurityKeypadViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,10 +18,17 @@ class SecurityKeypadViewModel @Inject constructor() : ViewModel() {
     private val _keypadNavigation : MutableSharedFlow<KeypadType> = MutableSharedFlow<KeypadType>()
     val keypadNavigation : SharedFlow<KeypadType> = _keypadNavigation.asSharedFlow()
 
+    private val _passwordState : MutableStateFlow<PasswordState> = MutableStateFlow<PasswordState>(PasswordState.Init)
+    val passwordState : StateFlow<PasswordState> = _passwordState.asStateFlow()
+
     fun onPasswordRegisterButtonClick() {
         viewModelScope.launch {
             _keypadNavigation.emit(KeypadType.Register())
         }
+    }
+
+    fun setPassword(password : String) {
+        _passwordState.value = PasswordState.Setting(password)
     }
 
 }
