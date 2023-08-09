f3a8627f4edffee3f40d1e70d24620be947a4752
hegunhee <gunhee0072@gmail.com>
2023-08-09 17:27:16 +0900

    [security-keypad] feat : 비밀번호 검사를 위해서 Check class에 현재 비밀번호 변수 추가

--- app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/KeypadType.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/KeypadType.kt
@@ -4,5 +4,5 @@ sealed class KeypadType() {
 
     data class Register(val name : String = "인증번호 등록") : KeypadType()
 
-    data class Check(val name : String = "인증번호 확인") : KeypadType()
+    data class Check(val name : String = "인증번호 확인", val currentPassword : String) : KeypadType()
 }
