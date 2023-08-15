8b7792b9fcf34b34133643c6e823c78210dc8a72
hegunhee <gunhee0072@gmail.com>
2023-08-15 12:49:21 +0900

    [password-log] feat : PasswordLog xml과 fragment 추가

---
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/log/PasswordLogFragment.kt
@@ -0,0 +1,39 @@
+package com.hegunhee.sample_playground.feature.secretkeypad.log
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hegunhee.sample_playground.R
+import com.hegunhee.sample_playground.databinding.FragmentPasswordLogBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PasswordLogFragment : Fragment() {
+
+    private lateinit var viewDataBinding : FragmentPasswordLogBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_password_log,container,false)
+        viewDataBinding = FragmentPasswordLogBinding.bind(root)
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            it.getStringArray(argKey).let{
+                it
+            }
+        }
+    }
+
+    companion object{
+        const val argKey = "passwordLog"
+    }
+}
