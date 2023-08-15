3e98251fca7bb5d2aa1a3339e19581bb0ad8ce98
hegunhee <gunhee0072@gmail.com>
2023-08-15 16:25:14 +0900

    [password-log] feat : 로그 리스트에서 데이터들로 LogAdapter의 현재 데이터를 등록

--- app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/log/PasswordLogFragment.kt
+++ app/src/main/java/com/hegunhee/sample_playground/feature/secretkeypad/log/PasswordLogFragment.kt
@@ -6,26 +6,34 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.hegunhee.sample_playground.R
 import com.hegunhee.sample_playground.databinding.FragmentPasswordLogBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class PasswordLogFragment : Fragment() {
 
     private lateinit var viewDataBinding : FragmentPasswordLogBinding
     private val viewModel : PasswordLogViewModel by viewModels()
+    private lateinit var logAdapter : LogAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        logAdapter = LogAdapter()
         val root = inflater.inflate(R.layout.fragment_password_log,container,false)
         viewDataBinding = FragmentPasswordLogBinding.bind(root).apply {
+            lifecycleOwner = viewLifecycleOwner
             viewModel = this@PasswordLogFragment.viewModel
+            logRecyclerView.adapter = logAdapter
         }
         return root
     }
@@ -33,11 +41,25 @@ class PasswordLogFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         arguments?.let {
-            it.getStringArray(argKey)?.map { toString() }?.let{ logList ->
+            it.getStringArray(argKey)?.let {
+                val logList = it.toList().map { it.toString() }
                 viewModel.fetchData(logList)
             }
         }
         viewDataBinding.toolbar.setupWithNavController(findNavController())
+        observeData()
+    }
+
+    private fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launch{
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.logList.collect {
+                        logAdapter.submitList(it)
+                    }
+                }
+            }
+        }
     }
 
     companion object{
